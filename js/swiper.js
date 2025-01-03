7d99d20e4352f18a2f48e06075cf61f7eccdbd0f
Roman Vashchenko <roman1991dp@gmail.com>
2025-01-03 09:03:52 +0200

    update swiper

--- js/swiper.js
+++ js/swiper.js
@@ -1,9 +1,9 @@
 const swiper = new Swiper(".swiper", {
   slidesPerView: 1,
   spaceBetween: 20,
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
+  pagination: {
+    el: ".swiper-pagination",
+    dynamicBullets: true,
   },
   breakpoints: {
     768: {

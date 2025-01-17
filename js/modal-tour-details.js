0eda54ec98eb2191631eac2ce4264eba7dfde7f9
Roman Vashchenko <roman1991dp@gmail.com>
2025-01-17 15:24:04 +0200

    update

--- js/modal-tour-details.js
+++ js/modal-tour-details.js
@@ -4,19 +4,28 @@ const refs = {
   closeModalHoverla: document.querySelector(
     ".js-modal-details-tour-hoverla-close-btn"
   ),
-  bookTourHoverla: document.querySelector(".js-modal-details-book-tour-btn"),
-  // modalBukovel: document.querySelector(".js-modal-details-tour-bukovel"),
-  // openModalBukovel: document.querySelector(".js-tours-btn-bukovel"),
-  // closeModalBukovel: document.querySelector(
-  //   ".js-modal-details-tour-bukovel-close-btn"
-  // ),
-  // modalCarpathians: document.querySelector(".js-modal-details-tour-carpathians"),
-  // openModalCarpathians: document.querySelector(".js-tours-btn-carpathians"),
-  // closeModalCarpathians: document.querySelector(
-  //   ".js-modal-details-tour-carpathians-close-btn"
-  // ),
+  bookTourHoverla: document.querySelector(
+    ".js-modal-details-book-tour-hoverla-btn"
+  ),
+  modalBukovel: document.querySelector(".js-modal-details-tour-bukovel"),
+  openModalBukovel: document.querySelector(".js-tours-btn-bukovel"),
+  closeModalBukovel: document.querySelector(
+    ".js-modal-details-tour-bukovel-close-btn"
+  ),
+  bookTourBukovel: document.querySelector(
+    ".js-modal-details-book-tour-bukovel-btn"
+  ),
 
-  // ),
+  modalCarpathians: document.querySelector(
+    ".js-modal-details-tour-carpathians"
+  ),
+  openModalCarpathians: document.querySelector(".js-tours-btn-carpathians"),
+  closeModalCarpathians: document.querySelector(
+    ".js-modal-details-tour-carpathians-close-btn"
+  ),
+  bookTourCarpathians: document.querySelector(
+    ".js-modal-details-book-tour-carpathians-btn"
+  ),
 };
 
 refs.openModalHoverla.addEventListener("click", () => {
@@ -31,5 +40,35 @@ refs.closeModalHoverla.addEventListener("click", () => {
 
 refs.bookTourHoverla.addEventListener("click", () => {
   refs.modalHoverla.classList.add("is-hidden");
+  document.body.style.overflow = "";
+});
+
+refs.openModalBukovel.addEventListener("click", () => {
+  refs.modalBukovel.classList.remove("is-hidden");
+  document.body.style.overflow = "hidden";
+});
+
+refs.closeModalBukovel.addEventListener("click", () => {
+  refs.modalBukovel.classList.add("is-hidden");
+  document.body.style.overflow = "";
+});
+
+refs.bookTourBukovel.addEventListener("click", () => {
+  refs.modalBukovel.classList.add("is-hidden");
+  document.body.style.overflow = "";
+});
+
+refs.openModalCarpathians.addEventListener("click", () => {
+  refs.modalCarpathians.classList.remove("is-hidden");
+  document.body.style.overflow = "hidden";
+});
+
+refs.closeModalCarpathians.addEventListener("click", () => {
+  refs.modalCarpathians.classList.add("is-hidden");
+  document.body.style.overflow = "";
+});
+
+refs.bookTourCarpathians.addEventListener("click", () => {
+  refs.modalCarpathians.classList.add("is-hidden");
   document.body.style.overflow = "";
 });
